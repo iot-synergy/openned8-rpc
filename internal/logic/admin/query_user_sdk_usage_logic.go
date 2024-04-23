4cbb24e33da7460396f72fa0d52a5e33019b33c8
ZhiNen <a1679415186@163.com>
2024-04-23 14:48:51 +0800

    Update query_user_sdk_usage_logic.go

--- internal/logic/admin/query_user_sdk_usage_logic.go
+++ internal/logic/admin/query_user_sdk_usage_logic.go
@@ -2,8 +2,8 @@ package admin
 
 import (
 	"context"
+	"github.com/iot-synergy/openned8-rpc/ent"
 	"github.com/iot-synergy/openned8-rpc/ent/sdkusage"
-
 	"github.com/iot-synergy/openned8-rpc/internal/svc"
 	"github.com/iot-synergy/openned8-rpc/types/openned8"
 
@@ -25,13 +25,64 @@ func NewQueryUserSdkUsageLogic(ctx context.Context, svcCtx *svc.ServiceContext) 
 }
 
 func (l *QueryUserSdkUsageLogic) QueryUserSdkUsage(in *openned8.UserSdkUsageQueryReq) (*openned8.SdkUsage, error) {
-	data, err := l.svcCtx.DB.SdkUsage.Query().Where(sdkusage.UserIDEQ(in.UserId)).First(l.ctx)
+	tx, err := l.svcCtx.DB.Tx(l.ctx)
 	if err != nil {
 		return nil, err
+	}
+	data, err := QueryUserSdk(tx, in, l.ctx)
+	if err != nil {
+		err := tx.Rollback()
+		if err != nil {
+			l.Logger.Error("rollback fail:" + err.Error())
+		}
+		return nil, err
+	} else {
+		err := tx.Commit()
+		if err != nil {
+			l.Logger.Error("commit fail:" + err.Error())
+			return nil, err
+		}
 	}
 	return &openned8.SdkUsage{
 		UserId: data.UserID,
 		All:    data.All,
 		Used:   data.Used,
+	}, nil
+}
+
+func QueryUserSdk(tx *ent.Tx, in *openned8.UserSdkUsageQueryReq, ctx context.Context) (*ent.SdkUsage, error) {
+	count, err := tx.SdkUsage.Query().Where(sdkusage.UserIDEQ(in.UserId)).Count(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if count == 0 {
+		save, err := tx.SdkUsage.Create().
+			SetUserID(in.UserId).
+			SetAll(0).
+			SetUsed(0).
+			Save(ctx)
+		if err != nil {
+			return nil, err
+		}
+		return &ent.SdkUsage{
+			ID:        save.ID,
+			CreatedAt: save.CreatedAt,
+			UpdatedAt: save.UpdatedAt,
+			UserID:    save.UserID,
+			All:       save.All,
+			Used:      save.Used,
+		}, nil
+	}
+	first, err := tx.SdkUsage.Query().Where(sdkusage.UserIDEQ(in.UserId)).First(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &ent.SdkUsage{
+		ID:        first.ID,
+		CreatedAt: first.CreatedAt,
+		UpdatedAt: first.UpdatedAt,
+		UserID:    first.UserID,
+		All:       first.All,
+		Used:      first.Used,
 	}, nil
 }
